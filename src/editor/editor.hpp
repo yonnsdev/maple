8b0809d9e4b668b9fab172430d08f8b32b485409
YonnsDev <yonnsdev@gmail.com>
2023-01-04 22:24:05 +0900

    Update editor.hpp

--- src/editor/editor.hpp
+++ src/editor/editor.hpp
@@ -14,12 +14,26 @@ class Editor : protected Pane {
 public:
     Editor(int x, int y, int w, int h) : Pane{x, y, w, h} {}
 
-    void update() {}
+    void update() {
+        get_key();
+    }
+
+    void get_key() {
+        SDL_Keycode key = _event_handler.get_events().key.keysym.sym;
+        if (key == 127) {
+            _code.pop_back();
+        } else if (key >= 32 && key <= 126) {
+            char ch = key;
+            _code.push_back(key);
+        }
+    }
 
 private:
     Window &_window = Window::instantiate();
     SDL_Renderer *_renderer = _window.get_renderer();
     EventHandler &_event_handler = EventHandler::instantiate();
+
+    std::string _code = "";
 };
 
 #endif
